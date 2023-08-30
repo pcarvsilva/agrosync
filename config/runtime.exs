f80d11294a58da74efef90d6f98fdb1cf19aaac2
pcarvsilva <pcarvsilva@gmail.com>
2023-08-30 17:10:24 -0300

    hot fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,6 +16,7 @@ if config_env() == :prod do
 
   config :recomendation_front, RecomendationFront.Repo,
     ssl: true,
+    socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
