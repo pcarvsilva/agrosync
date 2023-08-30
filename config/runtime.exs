ff3711f8c58bc4c8481e7acc1f35ba7113519942
pcarvsilva <pcarvsilva@gmail.com>
2023-08-30 19:02:34 -0300

    hot fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,6 +17,7 @@ if config_env() == :prod do
   config :recomendation_front, RecomendationFront.Repo,
     adapter: Ecto.Adapters.Postgres,
     url: "postgresql://postgres:postgres@prod.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
+    ssl: true,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
