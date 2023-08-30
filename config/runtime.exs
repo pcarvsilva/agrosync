7f24744bf3df82295f1dd75d20b1102040307722
pcarvsilva <pcarvsilva@gmail.com>
2023-08-30 18:45:27 -0300

    hot fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,7 +16,7 @@ if config_env() == :prod do
 
   config :recomendation_front, RecomendationFront.Repo,
     adapter: Ecto.Adapters.Postgres,
-    url: "postgresql://postgres:postgres@prod.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
+    url: "postgres:postgres@prod.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     ssl: true,
     socket_options: [:inet6]
