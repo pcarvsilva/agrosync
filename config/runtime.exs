00e6701768cad0f8cd30d5e47a25def35ba14ee8
pcarvsilva <pcarvsilva@gmail.com>
2023-08-30 19:21:35 -0300

    hot fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,7 +16,8 @@ if config_env() == :prod do
 
   config :recomendation_front, RecomendationFront.Repo,
     adapter: Ecto.Adapters.Postgres,
-    url: "postgresql://postgres:postgres@prod.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
+    url:
+      "postgresql://postgres:postgres@database.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
     ssl: false,
     database: "database",
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
@@ -47,7 +48,8 @@ if config_env() == :prod do
   config :recomendation_front, RecomendationFront.Infrastructure.EventStore,
     ssl: false,
     serializer: Commanded.Serialization.JsonSerializer,
-    url: "postgresql://postgres:postgres@prod.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/database",
+    url:
+      "postgresql://postgres:postgres@database.cbfv9kxxicuy.sa-east-1.rds.amazonaws.com/event_store",
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # ## Using releases
